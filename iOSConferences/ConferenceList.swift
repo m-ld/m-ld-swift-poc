ff23940535095b1e4baf6efa67668c4d282f568a
George Svarovsky <gsvarovsky@users.noreply.github.com>
2022-07-19 07:55:23 +0100

    Finishing code commentary

--- iOSConferences/ConferenceList.swift
+++ iOSConferences/ConferenceList.swift
@@ -12,17 +12,11 @@ import Combine
 struct ConferenceList: View {
     /// Domain to work with (loading from JSON, for now)
     let domain: OrmDomain = JsonDomain()
-    /// Keeping track in the view of the available conferences, see body & onAppear()
+    /// Keeping track in the view of the visible loaded conferences, see
+    /// ``body`` & ``onAppear()``. If we wanted to limit the visible list (for
+    /// example with paging, or infinite scroll), this would be a subset of all
+    /// available conferences.
     @State var conferences: [Conference] = []
-
-    func conferenceRow(conference: Conference) -> some View {
-        NavigationLink(destination: ConferenceDetails(conference: conference)) {
-            VStack(alignment: .leading) {
-                Text(conference.name).font(.headline)
-                Text(conference.location).font(.subheadline)
-            }
-        }
-    }
 
     var body: some View {
         NavigationView {
@@ -34,24 +28,37 @@ struct ConferenceList: View {
         }
     }
 
+    private func conferenceRow(conference: Conference) -> some View {
+        NavigationLink(destination: ConferenceDetails(conference: conference)) {
+            VStack(alignment: .leading) {
+                Text(conference.name).font(.headline)
+                Text(conference.location).font(.subheadline)
+            }
+        }
+    }
+
     private func onAppear() async -> Set<AnyCancellable> {
         var subs = Set<AnyCancellable>()
         // Obtain a snapshot state to work with. Note this goes out of scope
         // synchronously at the end of this method, but that's OK because the
-        // state.all() query guarantees consistency regardless
+        // state.all() query guarantees consistency of its results regardless
         let state = await domain.state()
         if !Task.isCancelled {
+            // Retrieve all conferences from the state when the List appears
             state.all(of: Conference.self)
                 .sink(
                     receiveCompletion: { completion in
+                        // TODO: UI error handling
                         if case let .failure(error) = completion {
                             fatalError("\(error)")
                         }
                     },
                     receiveValue: { conference in
+                        // Add each existing conference to the UI-local array
                         if !conferences.contains(where: { $0.id == conference.id }) {
                             conferences.append(conference)
                         }
+                        // If a conference becomes deleted, remove it
                         conference.$deleted
                             .sink { deleted in
                                 if deleted {
